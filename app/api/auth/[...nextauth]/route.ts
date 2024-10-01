e9a2e70177b9e4011f6a499fd8b9d488e20fb4f3
Logan Olson <loganolsonn@gmail.com>
2024-10-01 17:01:17 -0500

    Fixing nextauth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,5 @@
 import NextAuth from 'next-auth';
-import Google from 'next-auth/providers/google';
-
-export const authOptions = {
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-    }),
-  ],
-};
+import { authOptions } from '@/lib/authOptions';
 
 export const handler = NextAuth(authOptions);
 
